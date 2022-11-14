202167a9ae66c7206e654588cf2f761cfad4d8b4
linxiaoxin <linxiaoxin@corp.netease.com>
2022-11-14 09:44:18 +0800

    fix:修改了一些提示

--- src/assets/js/injectFile1.js
+++ src/assets/js/injectFile1.js
@@ -16,6 +16,9 @@ function startMonitor() {
                 // 获取前5行计算，一次性也不可能来5单，有需要后面再改
                 const top5Item = [...tableEL.querySelectorAll('.anq-table-row')].splice(1, 5);
                 for (let i = 0; i < top5Item.length; i++) {
+                    // 标题
+                    const title = top5Item[i].querySelector('[class*=NameRow_title_]').innerText;
+
                     const nums = top5Item[i].querySelectorAll('[class*=reportConfig_tdNumber_]');
                     // 第一个是花费
                     const cost = nums[0].innerText;
@@ -23,17 +26,16 @@ function startMonitor() {
                     const turnover = nums[1].innerText;
                     if (parseFloat(cost) > 20 || parseFloat(turnover) > 50) {
                         // 删除该项
-                        console.log('**** 删除数据 *****');
+                        console.warn(`**** ${title} 花费：${cost} 交易额：${turnover} DEL ${new Date().toLocaleTimeString()} ****`);
                         top5Item[i].querySelector('.anq-dropdown-trigger').click();
                         document.querySelector('[class*=OperationsNewRow_moreOperationsItem_]:last-child').click();
                         document.querySelector('.anq-btn-dangerous').click();
                         break;
                     } else {
-                        console.log(`==== 第${i+1}行数据 花费：${cost} 交易额：${turnover} PASS ====`)
+                        console.log(`==== ${title} 花费：${cost} 交易额：${turnover} PASS ====`)
                     }
                 }
 
-                console.log('end...');
                 // 自循环
                 startMonitor(); 
             }
