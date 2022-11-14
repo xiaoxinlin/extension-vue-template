821444d54e3776b6685b4be79e36b0697b279e7a
linxiaoxin <linxiaoxin@corp.netease.com>
2022-11-14 21:22:40 +0800

    fix:优化花费限制

--- src/assets/js/injectFile1.js
+++ src/assets/js/injectFile1.js
@@ -1,4 +1,4 @@
-console.log('自动监控开始！每次只监控前5条数据，花费>20或者交易额>50就删除数据，监控间隔5秒钟~');
+console.log('自动监控开始！每次只监控前5条数据，花费>3或者交易额>50就删除数据，监控间隔5秒钟~');
 function startMonitor() {
     setTimeout(() => {
         console.log('start....');
@@ -11,7 +11,7 @@ function startMonitor() {
                 // 数据更新完了
                 clearInterval(timer);
 
-                // 获取列表计算数字是否超标（花费 > 20 || 交易额 > 50）
+                // 获取列表计算数字是否超标（花费 > 3 || 交易额 > 50）
                 const tableEL = document.querySelector('.anq-table-tbody');
                 // 获取前5行计算，一次性也不可能来5单，有需要后面再改
                 const top5Item = [...tableEL.querySelectorAll('.anq-table-row')].splice(1, 5);
@@ -24,7 +24,7 @@ function startMonitor() {
                     const cost = nums[0].innerText;
                     // 第二个是交易额
                     const turnover = nums[1].innerText;
-                    if (parseFloat(cost) > 20 || parseFloat(turnover) > 50) {
+                    if (parseFloat(cost) > 3 || parseFloat(turnover) > 50) {
                         // 删除该项
                         console.warn(`**** ${title} 花费：${cost} 交易额：${turnover} DEL ${new Date().toLocaleTimeString()} ****`);
                         top5Item[i].querySelector('.anq-dropdown-trigger').click();
