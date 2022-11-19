cd380ea815d6919f243094ff0a8e126a692ce7a2
linxiaoxin <linxiaoxin@corp.netease.com>
2022-11-19 23:02:13 +0800

    feat: 去掉花费监控

--- src/assets/js/injectFile1.js
+++ src/assets/js/injectFile1.js
@@ -1,4 +1,4 @@
-console.log('自动监控开始！每次只监控前5条数据，花费>3或者交易额>50就删除数据，监控间隔5秒钟~');
+console.log('自动监控开始！每次只监控前5条数据，交易额>30就删除数据，监控间隔5秒钟~');
 function startMonitor() {
     setTimeout(() => {
         console.log('start....');
@@ -6,14 +6,12 @@ function startMonitor() {
         document.querySelector('[class*=CustomTable_filterPanelWrapper_] .anq-dropdown-trigger').click();
         document.querySelector('[class*=CustomTable_filterPanelWrapper_] [class*=CustomSelect_option_]:nth-child(2)').click();
 
-        // 重试次数
-        let retryCount = 0;
         const timer = setInterval(() => {
             if (!document.querySelector('[class*="anq-spin-container anq-spin-blur"]')) {
                 // 数据更新完了
                 clearInterval(timer);
 
-                // 获取列表计算数字是否超标（花费 > 3 || 交易额 > 50）
+                // 获取列表计算数字是否超标（交易额 > 30）
                 const tableEL = document.querySelector('.anq-table-tbody');
                 // 获取前5行计算，一次性也不可能来5单，有需要后面再改
                 const top5Item = [...tableEL.querySelectorAll('.anq-table-row')].splice(1, 5);
@@ -26,7 +24,7 @@ function startMonitor() {
                     const cost = nums[0].innerText;
                     // 第二个是交易额
                     const turnover = nums[1].innerText;
-                    if (parseFloat(cost) > 3 || parseFloat(turnover) > 50) {
+                    if (parseFloat(turnover) > 30) {
                         // 删除该项
                         console.warn(`**** ${title} 花费：${cost} 交易额：${turnover} DEL ${new Date().toLocaleTimeString()} ****`);
                         top5Item[i].querySelector('.anq-dropdown-trigger').click();
@@ -40,16 +38,6 @@ function startMonitor() {
 
                 // 自循环
                 startMonitor(); 
-            } else {
-                retryCount++;
-                // 10秒如果没响应提示
-                if (retryCount > 200) {
-                    // 重置重试次数
-                    retryCount = 0;
-                    clearInterval(timer);
-                    // 新开标签页
-                    window.open('//www.baidu.com');
-                }
             }
         }, 50);
     }, 5000);
